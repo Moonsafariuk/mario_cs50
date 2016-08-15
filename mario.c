08509e17831ce4f361500cf85ebec74b449f8fdb
Alex <alexanderchapman@outlook.com>
2016-08-15 16:04:27 +0100

    Added code.
    Uses stdio.h and cs50.h
    cs50.h is from the course and includes the GetInt() function.

--- mario.c
+++ mario.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
+#include <cs50.h>
 
+int main(void)
+{
+
+    int userHeight = 0;
+    int pyramidHeight = 0;
+    int row = 0;
+    int spaces;
+    int hashes;
+
+
+    do
+    {
+        printf("Please enter half the pyramid's height.. you can choose from 1 to 23\n");
+
+        userHeight = GetInt();
+
+    }
+
+    while(userHeight < 1 || userHeight > 23);
+
+    pyramidHeight = userHeight * 2 ;
+
+
+    printf("Total height: %d\n", pyramidHeight);
+
+
+    for (row = 1 ; row <= pyramidHeight ; row ++)
+    {
+
+         for (spaces=0; spaces <= pyramidHeight-row-1; spaces++ )
+        {
+           printf(" ");
+        }
+
+         for (hashes=0; hashes <= row; hashes++ )
+        {
+           printf("#");
+        }
+
+        printf("\n");
+
+    }
+
+}
